c14b8ffa7db3a115a15c768699ed6a3001a32874
Arshia <arshia.zargaran@gmail.com>
2024-11-10 13:40:50 -0800

    urls added for register, a temp veiw added in register for test,

--- register/views.py
+++ register/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from landing.models import Athlete
 # Create your views here.
+
+
+def temp(request):
+
+    # query = Athlete.objects.get(pk=1)
+    # a = list(query)
+    return render(request, 'temp.html', {'name': a[1].name})
