5dbe47f4e562d4f7b4d20f82079bf8f4d79d93de
Ani-nem <anunem2@Gmail.com>
2024-11-10 13:41:41 -0800

    finished basketballinfo model

--- landing/models.py
+++ landing/models.py
@@ -21,41 +21,58 @@ class Athlete(models.Model):
     def __str__(self) -> str:
         return self.first_name + ' ' + self.last_name
 
-# class BasketballInfo(models.Model):
+class BasketballInfo(models.Model):
+    #Experience
+    EXPERIENCE_PRO = 'P'
+    EXPERIENCE_COLLEGE = 'C'
+    EXPERIENCE_HIGHSCHOOL = 'H'
+    EXPERIENCE_STREET = 'S'
+    EXPERIENCE_BOOTCAMP = 'B'
 
-#     EXPERIENCE_PRO = 'P'
-#     EXPERIENCE_COLLEGE = 'C'
-#     EXPERIENCE_HIGHSCHOOL = 'H'
-#     EXPERIENCE_STREET = 'S'
-#     EXPERIENCE_BOOTCAMP = 'B'
+    EXPERIENCE_CHOICES = [
+        (EXPERIENCE_PRO, 'Pro'),
+        (EXPERIENCE_COLLEGE, 'College'),
+        (EXPERIENCE_HIGHSCHOOL, 'Highschool'),
+        (EXPERIENCE_STREET, 'Street'),
+        (EXPERIENCE_BOOTCAMP, 'Bootcamp'),
+    ]
 
-#     EXPERIENCE_CHOICES = [
-#         (EXPERIENCE_PRO, 'Pro'),
-#         (EXPERIENCE_COLLEGE, 'College'),
-#         (EXPERIENCE_HIGHSCHOOL, 'Highschool'),
-#         (EXPERIENCE_STREET, 'Street'),
-#         (EXPERIENCE_BOOTCAMP, 'Bootcamp'),
-#     ]
 
-#     POSITION_POINTGURD = 'PG'
-#     POSITION_SHOOTINGGUARD = 'SG'
-#     POSITION_SMALLFORWARD = 'SF'
-#     POSITION_POWERFORWARD = 'PF'
-#     POSITION_CENTER = 'CR'
+    #Positons
+    POSITION_POINTGURD = 'PG'
+    POSITION_SHOOTINGGUARD = 'SG'
+    POSITION_SMALLFORWARD = 'SF'
+    POSITION_POWERFORWARD = 'PF'
+    POSITION_CENTER = 'CR'
 
-#     POSITION_CHOICES = [
-#         (POSITION_POINTGURD, 'Point guard'),
-#         (POSITION_SHOOTINGGUARD, 'Shooting guard'),
-#         (POSITION_SMALLFORWARD, 'Small forward'),
-#         (POSITION_POWERFORWARD, 'Power forward'),
-#         (POSITION_CENTER, 'Center'),
-#     ]
+    POSITION_CHOICES = [
+        (POSITION_POINTGURD, 'Point guard'),
+        (POSITION_SHOOTINGGUARD, 'Shooting guard'),
+        (POSITION_SMALLFORWARD, 'Small forward'),
+        (POSITION_POWERFORWARD, 'Power forward'),
+        (POSITION_CENTER, 'Center'),
+    ]
 
-#     athlete = models.ForeignKey(Athlete, on_delete=models.PROTECT)
-#     experience = models.CharField(
-#         max_length=1, choices=EXPERIENCE_CHOICES)
-#     positiom = models.ForeignKey(Athlete, on_delete=models.PROTECT)
-#     experience = models.CharField(
-#         max_length=2, choices=POSITION_CHOICES)
-    # frequency_of_play =
-    # favourite_tean =
+    #Frequencies
+    FREQUENCY_OFTEN = 'O'
+    FREQUENCY_WEEKLY = 'W'
+    FREQUENCY_MONTHLY = 'M'
+    FREQUENCY_RARELY = 'R'
+
+    FREQUENCY_CHOICES = [
+        (FREQUENCY_OFTEN, 'Often'),
+        (FREQUENCY_WEEKLY,'Weekly'),
+        (FREQUENCY_MONTHLY, 'Monthly'),
+        (FREQUENCY_RARELY, 'Rarely'),
+    ]
+
+    athlete = models.ForeignKey(Athlete, on_delete=models.PROTECT)
+    experience = models.CharField(
+        max_length=1, choices=EXPERIENCE_CHOICES)
+    positiom = models.ForeignKey(Athlete, on_delete=models.PROTECT)
+    experience = models.CharField(
+        max_length=2, choices=POSITION_CHOICES)
+    frequency_of_play = models.CharField(
+        max_length=1, choices=FREQUENCY_CHOICES
+    )
+    favourite_team = models.CharField(max_length=255)
